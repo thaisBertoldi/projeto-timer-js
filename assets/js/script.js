34c45342de56124227dca728f413cd2c6b9dc256
Thais Bertoldi <thais_brt@hotmail.com>
2021-11-11 15:13:25 -0300

    Evento iniciar

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+function criaHoraDosSegundos(segundos) {
+    const data = new Date(segundos * 1000);
+    return data.toLocaleTimeString('pt-BR', {
+        hour12: false,
+        timeZone: 'GMT'
+    });
+}
+
+const relogio = document.querySelector('.relogio');
+const iniciar = document.querySelector('.iniciar');
+const pausar = document.querySelector('.pausar');
+const zerar = document.querySelector('.zerar');
+let segundos = 0;
+
+function iniciaRelogio(){
+    const timer = setInterval(function(){
+        segundos++;
+        relogio.innerHTML = criaHoraDosSegundos(segundos);
+    }, 1000);
+}
+
+iniciar.addEventListener('click', function(e) {
+    iniciaRelogio();
+});
