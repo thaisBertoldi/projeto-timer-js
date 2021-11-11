77448589e1a5bee78eb69e5e7768b7e5ccb1e3fa
Thais Bertoldi <thais_brt@hotmail.com>
2021-11-11 15:31:31 -0300

    Correções

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,9 +7,6 @@ function criaHoraDosSegundos(segundos) {
 }
 
 const relogio = document.querySelector('.relogio');
-const iniciar = document.querySelector('.iniciar');
-const pausar = document.querySelector('.pausar');
-const zerar = document.querySelector('.zerar');
 let segundos = 0;
 let timer;
 
